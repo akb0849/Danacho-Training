9ba7c9f12f926e610854e0cee30493de5aaaab74
reactDev777 <91951862+reactDev777@users.noreply.github.com>
2022-03-18 16:51:44 +0500

    title issue fixed

--- src/subComponents/collapsibleContainer/collabsibleContainer.tsx
+++ src/subComponents/collapsibleContainer/collabsibleContainer.tsx
@@ -1,33 +1,43 @@
-import React, { useState } from 'react'
-import { IProps, Wrapper,Content,ContentContainer,ContentWrapper,OptionContainer,OptionWrapper,Title,TriggerButton, OpenIcon, CloseIcon } from './collapsibleContainerElements';
+import React, { useState } from "react";
+import {
+  IProps,
+  Wrapper,
+  Content,
+  ContentContainer,
+  ContentWrapper,
+  OptionContainer,
+  OptionWrapper,
+  Title,
+  TriggerButton,
+  OpenIcon,
+  CloseIcon,
+} from "./collapsibleContainerElements";
 const CollapsibleContainer: React.FC<IProps> = ({ open, children, title }) => {
-    const [isOpen, setIsOpen] = useState(open);
-  
-    const handleFilterOpening = () => {
-      setIsOpen((prev) => !prev);
-    };
+  const [isOpen, setIsOpen] = useState(open);
+  console.log(isOpen);
+  const handleFilterOpening = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
     <>
       <Wrapper>
         <OptionWrapper>
-        <Title>{title}</Title>
+          <Title>{title}</Title>
           <OptionContainer>
             <TriggerButton onClick={handleFilterOpening}>
-              {!isOpen ? (
-                <OpenIcon />
-              ) : (
-                <CloseIcon />
-              )}
+              {!isOpen ? <OpenIcon /> : <CloseIcon />}
             </TriggerButton>
           </OptionContainer>
         </OptionWrapper>
 
         <ContentWrapper className="border-bottom">
-          <ContentContainer>{isOpen && <Content>{children}</Content>}</ContentContainer>
+          <ContentContainer>
+            {isOpen && <Content>{children}</Content>}
+          </ContentContainer>
         </ContentWrapper>
       </Wrapper>
     </>
-  )
-}
+  );
+};
 
-export default CollapsibleContainer
+export default CollapsibleContainer;
--- src/subComponents/collapsibleContainer/collapsibleContainerElements.ts
+++ src/subComponents/collapsibleContainer/collapsibleContainerElements.ts
@@ -1,92 +1,116 @@
 import styled from "styled-components";
-import {Plus} from '@styled-icons/bootstrap/Plus'
-import {ChevronUp} from '@styled-icons/bootstrap/ChevronUp'
+import { Plus } from "@styled-icons/bootstrap/Plus";
+import { ChevronUp } from "@styled-icons/bootstrap/ChevronUp";
 export interface IProps {
-    open?: boolean;
-    title: string;
+  open?: boolean;
+  title: string;
 }
-  
+
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+
+  @media (max-width: 768px) {
+    &:first-child,
+    :nth-of-type(2),
+    :nth-of-type(3),
+    :nth-of-type(4),
+    :nth-of-type(5) {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      font-size: 20px;
+    }
+  }
+`;
+
+export const TriggerButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
+  width: 100%;
+`;
+
+// The only way counter this issue is by fixed width of the title and by fixed with of the title you have to put a lot of
+// queries to make responsive i have added some of them. Thank you.
+
+export const Title = styled.div`
+  font-weight: bold;
+  font-size: 20px;
+  width: 32rem;
+
+
+
+  @media (max-width: 768px) {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin-top: 20px;
-    margin-bottom: 20px;
+  }
+  @media (max-width: 567px) {
+    width: 31rem;
+  }
+  @media (max-width: 540px) {
+    width: 30rem;
+    margin-left: -1.5rem;
+    font-size: 15px;
+  }
 
-    @media (max-width: 768px) {
-        &:first-child,
-        :nth-of-type(2),
-        :nth-of-type(3),
-        :nth-of-type(4),
-        :nth-of-type(5){
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            font-size: 20px;
-        }        
-    }
+  @media (max-width: 500px) {
+    width: 25rem;
+    margin-left: -1.5rem;
+    font-size: 15px;
+  }
 
-`
-
-
-export const TriggerButton = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    justify-content: flex-end;
-    width: 100%;
-`
-
-export const Title = styled.div`
-    font-weight: bold;
-    font-size: 20px;
-    @media (max-width: 768px) {
-        display: flex;
-        flex-direction:column;
-        align-items: center;
-        justify-content: center;
-    }
-`
+  @media (max-width: 414px) {
+    width: 24.5rem;
+    margin-left: -1.5rem;
+    font-size: 14px;
+  }
+  
+`;
 export const OptionWrapper = styled.div`
-    display: flex;
-    
-`
+  display: flex;
+`;
 
 export const OptionContainer = styled.div`
-    display: flex;
-    
-`
+  display: flex;
+`;
 
-export const ContentWrapper = styled.div``
+export const ContentWrapper = styled.div``;
 
-export const ContentContainer = styled.div``
+export const ContentContainer = styled.div``;
 
 export const Content = styled.div`
-    width: 500px;
-    margin-top: 10px;
-    margin-left: 20px;
-    display: flex;
-    flex-direction:column;
-    align-items: flex-start;
-    justify-content: flex-start;
+  width: 500px;
+  margin-top: 10px;
+  margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
 
-    @media (max-width: 768px) {
-        width: 90%;        
-    }
-`
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`;
 
 export const OpenIcon = styled(Plus)`
-    color: black;
-    height: 20px;
-    width: 20px;
-    margin-left: 20px;
-`
+  color: black;
+  height: 20px;
+  width: 20px;
+  margin-left: 20px;
+`;
 
 export const CloseIcon = styled(ChevronUp)`
-    color: black;
-    height: 20px;
-    width: 20px;
-    margin-left: 20px;
-`
+  color: black;
+  height: 20px;
+  width: 20px;
+  margin-left: 20px;
+`;
